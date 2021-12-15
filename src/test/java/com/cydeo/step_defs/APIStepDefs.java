0d4076f0ce8454a6af3dd19d78e9600562bccdc1
Mehmet <mhmtsytrk@gmail.com>
2021-12-15 17:20:43 -0500

    API feature is DONE

--- src/test/java/com/cydeo/step_defs/APIStepDefs.java
+++ src/test/java/com/cydeo/step_defs/APIStepDefs.java
@@ -1,30 +1,50 @@
 package com.cydeo.step_defs;
 
+import com.cydeo.utility.ConfigReader;
+import com.cydeo.utility.LibraryAPI_Util;
 import io.cucumber.java.en.*;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class APIStepDefs {
+    RequestSpecification givenPart;
+    Response response;
+    ValidatableResponse thenPart;
+
+
     @Given("the base_uri and base_path set")
     public void the_base_uri_and_base_path_set() {
+        baseURI= ConfigReader.read("library.baseUri");
+
+        givenPart=given().log().uri();
 
     }
     @Given("I logged in as {string}")
-    public void i_logged_in_as(String string) {
-
+    public void i_logged_in_as(String user) {
+        givenPart.header("x-library-token", LibraryAPI_Util.getToken(user));
     }
     @Given("I create a random book")
     public void i_create_a_random_book() {
-
+        givenPart.formParams(LibraryAPI_Util.getRandomBookMap());
     }
     @When("I send post request to {string} endpoint")
-    public void i_send_post_request_to_endpoint(String string) {
+    public void i_send_post_request_to_endpoint(String endpoint) {
+         response = givenPart.when().post(endpoint);
 
+         thenPart= response.then();
     }
     @Then("I should get status code {int}")
-    public void i_should_get_status_code(Integer int1) {
-
+    public void i_should_get_status_code(Integer expectedStatusCode) {
+        thenPart.statusCode(expectedStatusCode);
     }
     @Then("the field value for {string} path should be equal to {string}")
-    public void the_field_value_for_path_should_be_equal_to(String string, String string2) {
+    public void the_field_value_for_path_should_be_equal_to(String path, String expectedResult) {
+
+        thenPart.body(path, is(expectedResult));
 
     }
 }
