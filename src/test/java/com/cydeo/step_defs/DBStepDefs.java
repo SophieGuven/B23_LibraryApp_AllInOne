231822524c3c1e2203b4ae707303f5b8124e759b
Mehmet <mhmtsytrk@gmail.com>
2021-12-15 16:38:44 -0500

    DB feature is started

--- src/test/java/com/cydeo/step_defs/DBStepDefs.java
+++ src/test/java/com/cydeo/step_defs/DBStepDefs.java
@@ -1,11 +1,17 @@
 package com.cydeo.step_defs;
 
+import com.cydeo.utility.DB_Util;
+import com.cydeo.utility.LibraryDB_Util;
 import io.cucumber.java.en.*;
 
 public class DBStepDefs {
 
     @Then("the information should be same with database  {string}")
     public void the_information_should_be_same_with_database(String moduleName) {
+
+
+        String moduleQuery = LibraryDB_Util.getModuleQuery(moduleName);
+        DB_Util.runQuery(moduleQuery);
 
     }
 
